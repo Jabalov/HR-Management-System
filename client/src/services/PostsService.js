ea130ce2290d970b800766857a35b8f8200ad3eb
Jabalov <moh.aboajabl@gmail.com>
2019-05-08 18:05:22 +0200

    يا دين أمي ع العك

---
+++ client/src/services/PostsService.js
@@ -0,0 +1,24 @@
+import Api from '@/services/Api'
+
+export default {
+  fetchPosts () {
+    return Api().get('posts')
+  },
+
+  addPost (params) {
+    return Api().post('posts/add_post', params)
+  },
+
+  updatePost (params) {
+    return Api().put('posts/' + params.id, params)
+  },
+
+  getPost (params) {
+    return Api().get('posts/post/' + params.id)
+  },
+
+  deletePost (id) {
+    return Api().delete('posts/' + id)
+  }
+}
+
